332d0cc20ff05782b9d7226316a548669095d392
Yasir Hossain <yasir.hossain@theplatform.com>
2016-10-12 12:31:42 -0700

    tweaked naming

--- src/texttrackrenderer.js
+++ src/texttrackrenderer.js
@@ -1,15 +1,15 @@
 export default class TextTrackRenderer {
   constructor = () => {}
 
-  attach = (element) => {
-    this.element = element;
+  attach = (divElement) => {
+    this.divElement = divElement;
     let string = 'Hello World!';
-    this.element.innerHTML= `<span>${string}</span>`;
+    this.divElement.innerHTML= `<span>${string}</span>`;
   }
 
   addTextTrack = (trackElement) => {
     this.trackElement = trackElement;
-    this.trackElement.addEventListener("load", function() {
+    this.trackElement.addEventListener('load', function() {
       const textTrack = this.track;
       for (let cue of textTrack.cues) {
         console.log(cue);
