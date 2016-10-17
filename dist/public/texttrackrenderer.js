da1cca35bf3e06e4d9c35baeb638fd9bccc07a26
Yasir Hossain <yasir.hossain@theplatform.com>
2016-10-17 16:33:59 -0700

    new text track renderer

--- dist/public/texttrackrenderer.js
+++ dist/public/texttrackrenderer.js
@@ -57,46 +57,55 @@ var TextTrackRenderer =
 	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 	
 	var TextTrackRenderer = function TextTrackRenderer() {
-	  var divElement = void 0;
+	  var div = void 0,
+	      track = void 0;
 	
-	  var attach = function attach(div) {
-	    if (div.tagName) divElement = div;else console.log(div + ' is not an HTML div container');
+	  var attach = function attach(element) {
+	    if (element.tagName) div = element;else console.log(div + ' is not an HTML div container');
 	  };
 	
-	  var addTextTrack = function addTextTrack(track) {
-	    if (divElement == null) console.log('attach div container using .attach() first');
-	    if (track == null) console.log('please add track first');else loadTrack(track);
+	  var addTextTrack = function addTextTrack(obj) {
+	    if (div == null) console.log('attach div container using .attach() first');
+	    if (obj == null) console.log('please add track first');else {
+	      track = obj;
+	      if (isElement(track)) loadTrack(track, track.track);else loadTrack(document.querySelector('track'), track); // <-- This should be a more precise selector
+	    }
 	  };
 	
-	  var loadTrack = function loadTrack(track) {
-	    var trackObj = track;
-	    if (!isElement(trackObj) && (typeof trackObj === 'undefined' ? 'undefined' : _typeof(trackObj)) == 'object') renderCues(trackObj);else if (trackObj.readyState == 2) renderCues(trackObj.track);else console.log('The ' + (typeof trackObj === 'undefined' ? 'undefined' : _typeof(trackObj)) + ' "' + trackObj + '" is not a valid track');
+	  var loadTrack = function loadTrack(elem, obj) {
+	    var trackElem = elem,
+	        trackObj = obj;
+	    if (trackElem.readyState == 2) renderCues(trackObj);else if (trackElem.addEventListener) trackElem.addEventListener('load', function () {
+	      loadTrack(trackElem, trackObj);
+	    }, false);else trackElem.attachEvent('onload', function () {
+	      loadTrack(trackElem, trackObj);
+	    });
 	  };
 	
 	  var renderCues = function renderCues(obj) {
 	    var trackObj = obj;
-	    var _iteratorNormalCompletion = true;
-	    var _didIteratorError = false;
-	    var _iteratorError = undefined;
+	    if ((typeof trackObj === 'undefined' ? 'undefined' : _typeof(trackObj)) !== 'object') console.log('The ' + (typeof trackObj === 'undefined' ? 'undefined' : _typeof(trackObj)) + ' "' + trackObj + '" is not a valid track');else {
+	      var _iteratorNormalCompletion = true;
+	      var _didIteratorError = false;
+	      var _iteratorError = undefined;
 	
-	    try {
-	      for (var _iterator = trackObj.cues[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	        var cue = _step.value;
-	
-	        divElement.innerHTML = divElement.innerHTML + ('<span>' + cue.text + '</span>');
-	        console.log(cue);
-	      }
-	    } catch (err) {
-	      _didIteratorError = true;
-	      _iteratorError = err;
-	    } finally {
 	      try {
-	        if (!_iteratorNormalCompletion && _iterator.return) {
-	          _iterator.return();
+	        for (var _iterator = trackObj.cues[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	          var _cue = _step.value;
+	          div.innerHTML = div.innerHTML + ('<span>' + _cue.text + '</span>');
 	        }
+	      } catch (err) {
+	        _didIteratorError = true;
+	        _iteratorError = err;
 	      } finally {
-	        if (_didIteratorError) {
-	          throw _iteratorError;
+	        try {
+	          if (!_iteratorNormalCompletion && _iterator.return) {
+	            _iterator.return();
+	          }
+	        } finally {
+	          if (_didIteratorError) {
+	            throw _iteratorError;
+	          }
 	        }
 	      }
 	    }
