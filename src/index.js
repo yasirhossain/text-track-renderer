1706b2c4067a180d93836e7debb772fa5b25a5ef
Yasir Hossain <yasir.hossain@theplatform.com>
2016-10-26 15:05:10 -0700

    added styling and two view buttons

--- src/index.js
+++ src/index.js
@@ -1,90 +1,67 @@
 import TextTrackRenderer from './texttrackrenderer'
-// import Hls from 'hls.js'
+
+let video = document.querySelector('video'),
+    videoSample = {
+      videos: [
+        {
+          src: 'http://pmd205470tn.download.theplatform.com.edgesuite.net/Jeremy_LaCivita_(VMS)/654/339/developerStories-en.mp4',
+          type: `video/webm; codecs='vp8, vorbis'`
+        },
+        {
+          src: 'http://pmd205470tn.download.theplatform.com.edgesuite.net/Jeremy_LaCivita_(VMS)/654/339/developerStories-en.webm',
+          type: `video/mp4;`
+        },
+      ],
+      tracks: [
+        {
+          src: '/dist/public/video/subs-en.vtt',
+          label: 'English subtitles',
+          kind: 'subtitles',
+          srclang: 'en'
+        }
+      ]
+    },
+    d = document.getElementById('trackRenderer'),
+    t = document.querySelector('track'),
+    vt = document.querySelector('video').textTracks[0]
 
 // Instantiates TextTrackRenderer
 const ttr = new TextTrackRenderer()
-
-// User chooses div to pass to attach handler
-let d = document.getElementById('trackRenderer')
 ttr.attach(d)
 
-// Adds TextTrack
-let t = document.querySelector('track')
-let vt = document.querySelector('video').textTracks[0]
+const defaultLoader = () => {
+    video.removeAttribute('src')
+    video.pause()
+    video.load()
+    video.play()
+    video.innerHTML = ''
+    for (let source of videoSample.videos)
+      video.innerHTML += `<source src="${source.src}" type="${source.type}"></source>`
+    for (let track of videoSample.tracks)
+      video.innerHTML += `<track src="${track.src}" label="${track.label}" kind="${track.kind}" srclang="${track.srclang}" default></track>`
+    setTimeout(function() {
+      ttr.setTextTrack(document.querySelector('video').textTracks[0])
+    }, 1000)
+}
+
+const hlsLoader = () => {
+  if(Hls.isSupported()) {
+    let hls = new Hls()
+    video.innerHTML = ''
+    hls.loadSource('http://cdn3.videos.bloomberg.com/btv/us/master.m3u8')
+    hls.attachMedia(video)
+    hls.on(Hls.Events.MANIFEST_PARSED,function(event, data) {
+      video.play()
+      console.log(data)
+      setTimeout(function() {
+        ttr.setTextTrack(document.querySelector("video").textTracks[0]);
+      }, 5000)
+    })
+  }
+}
+
+document.getElementById('btn-default').addEventListener('click', defaultLoader)
+document.getElementById('btn-hls').addEventListener('click', hlsLoader)
 
 //ttr.setTextTrack(vt)
 //ttr.setTextTrack(t)
-
-if(Hls.isSupported()) {
-  var video = document.querySelector('video')
-  var hls = new Hls()
-  hls.loadSource('http://cdn3.videos.bloomberg.com/btv/us/master.m3u8')
-  hls.attachMedia(video)
-  hls.on(Hls.Events.MANIFEST_PARSED,function(event, data) {
-    video.play()
-    console.log(data)
-    setTimeout(function() {
-      ttr.setTextTrack(document.querySelector("video").textTracks[0]);
-    }, 10000)
-  })
-}
-
-/*
-setTimeout(function() {
-  ttr.setTextTrack(vt)
-}, 1000)
-*/
-
-// Below for notes only
-/*
-// With PDK Dependency
-$pdk.controller.addEventListener('OnTextTracksAvailable', function(e){
-  console.log(':::::::::::OnTextTracksAvailable:::::::::::');
-  var num =e.data.entries.length;
-  console.log(e);
-});
-*/
-
-/*
-// Proof of Concept Code
-let v = document.querySelector('video');
-
-const initTextTracks = () => {
-  let track = document.createElement('track');
-
-  track.src = '../video/developerStories-subtitles-en.vtt';
-  track.label = 'English subtitles';
-  track.kind = 'subtitles';
-  track.srclang = 'en';
-  track.mode = 'showing';
-  track.setAttribute('default', '');
-  v.appendChild(track);
-  v.setAttribute('crossOrigin', 'anonymous');
-};
-
-// IE11, Firefox, Chrome, Safari, iOS10, Android
-document.addEventListener('DOMContentLoaded', function() {
-  initTextTracks();
-
-  let textTracks = v.textTracks;
-  let textTrackContainer = document.createElement('div');
-  textTrackContainer.setAttribute('id', 'tpTextTrackRender');
-  document.getElementsByTagName('div')[0].appendChild(textTrackContainer);
-
-  let cueRenderer = () => {
-    const cue = v.textTracks[0].activeCues[0];
-    if (cue !== undefined) {
-      console.log(cue);
-      console.log(cue.text);
-      textTrackContainer.innerHTML = '<p>' + cue.text + '</p>';
-    }
-  };
-
-  if (textTracks.length > 0) {
-    textTracks[0].oncuechange = () => { cueRenderer(); };
-  } else {
-    // some browsers don't support track.oncuechange
-    v.addEventListener('timeupdate', () => { cueRenderer(); });
-  }
-});
-*/
